4cf698619374e0237f1f847ca2758bbc4117a305
archit <architchhajed6@gmail.com>
2023-08-16 18:23:48 +0530

    Added Coursebox, Added Login and Sign-Up Button To Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,65 +4,47 @@ import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import {Home,RoadMap,Login,SignUp} from './components'
 import "./App.css";
-<<<<<<< HEAD
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import LoginDiv from "./components/LoginDiv";
 import Newsbox from "./components/Newsbox";
 
-function App() {
-  return (
-    // <>
-    // <BrowserRouter>
-    // <Navbar />
-    //   <Routes>
-    //     {/* <Route path="/" element={<Layout />}> */} 
-    //       <Route index element={<Home />} />
-    //       <Route path="/course/:slug" element={<RoadMap/>}></Route>
-    //       {/* <Route path="blogs" element={<Blogs />} />
-    //       <Route path="contact" element={<Contact />} />
-    //     <Route path="*" element={<NoPage />} /> */}
-    //     {/* </Route> */}
-    //   </Routes>
-    // <Footer />
-    // </BrowserRouter>
-    // </>
-    // // <LoginDiv />
-
-
-    <Navbar />
-=======
-
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
 import AdminPanel from "./components/adminpanel"
+import CourseBox from "./components/courseBox";
 
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    {/* <Navbar /> */}
-      <Routes>
+    {/* <BrowserRouter>
+    // <Navbar />
+      <Routes> */}
       
         {/* <Route path="/" element={<Layout />}> */}
-          <Route path="/userprofile/:user" element={<AdminPanel/>}></Route>
+          {/* <Route path="/userprofile/:user" element={<AdminPanel/>}></Route>
 
           <Route index element={<Home />} />
           <Route path="/course/:slug" element={<RoadMap/>}/>
           <Route path ="/login" element={<Login/>}/>
-          <Route path ="/signup" element={<SignUp/>}/>
+          <Route path ="/signup" element={<SignUp/>}/> */}
 
           {/* <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NoPage />} /> */}
         {/* </Route> */}
-      </Routes>
+      {/* </Routes>
     <Footer />
-    </BrowserRouter>
+
+
+
+
+    </BrowserRouter> */}
+
+
+    <Navbar />
+
     </>
 
->>>>>>> 93d3a18e412a540228b6235dec16d542dade1a49
 
   )
 }
